f160821f818dfdc49fe9a1636d9e1b22311f9f6d
todos <nazmussakib409@gmail.com>
2024-03-30 03:46:23 +0600

    finally final commit

---
+++ employees_management-backend/src/main/java/ns11/employees_managementbackend/EmployeeManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ns11.employees_managementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementBackendApplication.class, args);
+	}
+
+}
---
+++ employees_management-backend/src/test/java/ns11/employees_managementbackend/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ns11.employees_managementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
